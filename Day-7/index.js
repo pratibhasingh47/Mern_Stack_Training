965c0032ecbde143f62d98e440b56c109255b532
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-13 15:00:35 +0530

    operators

--- Day-7/index.js
+++ Day-7/index.js
@@ -1,10 +1,37 @@
 console.log("Hello Pratibha");
 
-var a = [10,20,30,[],[],"Shubham"];
+// var a = [10, 20, 30, [], [], "Shubham"];
 
-console.log(a);
+// console.log(a);
+// console.log(typeof a);
+
+// JSON - JS Object Notation
 
 var a = {
-    firstName : "ABC",
-    lastName : "Jain"
+    firstName: "ABC",
+    lastName: "Jain"
 }
+
+var date = new Date();
+// console.log(typeof a);
+
+// var a = 1000;
+// var a = 90;
+
+let c = 20;
+c = 40;
+
+// const b= 40;
+
+// Airthematic operator
+
+let ax = 10;
+let b = 20;
+
+let sum = ax+b;
+let sub = ax-b;
+let mul = ax*b;
+let div = ax/b;
+let exp = ax**b;
+
+console.log(sum,sub,mul,div,exp);
