19129de08aeaccac7c0cb38fb0e3bdd6ad941def
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-14 14:05:54 +0530

    arrow fun

--- Day-8/index.js
+++ Day-8/index.js
@@ -15,3 +15,40 @@ else{
 // In ternary it is mandatory to include else condition
 
 
+let daynumber = 3;
+let dayName;
+
+switch(dayName){
+    case 1 :
+        dayName = "Monday";
+        break;
+    case 2 :
+        dayName = "Tuesday";
+        break;
+    case 3 :
+        dayName = "Wednesday";
+        break;
+    case 4 :
+        dayName = "Thursday";
+        break;
+    case 5 :
+        dayName = "Friday";
+        break;
+    case 6 :
+        dayName = "Saturday";
+        break;
+    case 7 :
+        dayName = "Sunday";
+        break;
+    default :
+        dayName = "Invalid Day";
+        break;
+}
+
+console.log(dayName);
+
+
+const sum = () =>{
+    console.log("This is arrow function");
+}
+
