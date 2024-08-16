a033b1c3b11ad176a6484dd1e89dea09013eea1d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-16 15:21:56 +0530

    js

--- Day-9/script.js
+++ Day-9/script.js
@@ -15,20 +15,49 @@
 //     count++;
 // }while(count <10); 
 
-let arr = ['A','B','C','D','E'];
-for(let i = 0; i < arr.length ; i++){
-    console.log(arr[i]);
-}
+// let arr = ['A','B','C','D','E'];
+// for(let i = 0; i < arr.length ; i++){
+//     console.log(arr[i]);
+// }
 
-for(let element of arr){
-    console.log(element);
-}
+// for(let element of arr){
+//     console.log(element);
+// }
 
-for(let index in arr){
-    console.log(index);
-}
+// for(let index in arr){
+//     console.log(index);
+// }
 
-let a = 20;
-b = 30;
-console.log(a);
-console.log(b);
+// let a = 20;
+// b = 30;
+// console.log(a);
+// console.log(b);
+
+// let data = [
+//     {
+//         name: "xyz" , city :"Jaipur" , dept : "FSD"
+//     },
+//     {
+//         name: "xyz" , city :"Jaipur" , dept : "FSD"
+//     },
+//     {
+//         name: "xyz" , city :"Jaipur" , dept : "FSD"
+//     }
+// ]
+// console.count("Counter");
+
+// Array Methods
+
+// let arr = [ 10,20,30,40,50,60,70];
+
+// push , pop , shift , unshift
+
+// arr.push(100);
+// arr.pop(50);
+// arr.unshift(40);
+// arr.shift(10);
+
+// console.log(arr);
+
+//Concat
+map return new arraywith the same length as original
