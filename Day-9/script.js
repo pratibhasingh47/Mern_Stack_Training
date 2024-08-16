be27c9c4560920395152fe2851f0df5396b34046
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-16 13:20:17 +0530

    script.js

---
+++ Day-9/script.js
@@ -0,0 +1,15 @@
+// loops ,  strict mode , console type, array methods
+
+// while , do-while,for
+
+let count = 1;
+
+while(Count <= 10){
+    console.log(count);
+    count++;
+}
+
+do{
+    console.log(count);
+    count++;
+}while(count <10); 
