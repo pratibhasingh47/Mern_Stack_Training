39d7ed96425c1fc4f8914cd68015c7fb0184b834
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-16 14:26:49 +0530

    js

--- Day-9/script.js
+++ Day-9/script.js
@@ -1,15 +1,34 @@
+// "use strict"
 // loops ,  strict mode , console type, array methods
 
 // while , do-while,for
 
-let count = 1;
+// let count = 1;
 
-while(Count <= 10){
-    console.log(count);
-    count++;
+// while(Count <= 10){
+//     console.log(count);
+//     count++;
+// }
+
+// do{
+//     console.log(count);
+//     count++;
+// }while(count <10); 
+
+let arr = ['A','B','C','D','E'];
+for(let i = 0; i < arr.length ; i++){
+    console.log(arr[i]);
 }
 
-do{
-    console.log(count);
-    count++;
-}while(count <10); 
+for(let element of arr){
+    console.log(element);
+}
+
+for(let index in arr){
+    console.log(index);
+}
+
+let a = 20;
+b = 30;
+console.log(a);
+console.log(b);
