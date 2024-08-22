42b3dbb8f2bb677d4962f46bf28e937603889259
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-22 22:45:36 +0530

    js

--- Day-10/index.js
+++ Day-10/index.js
@@ -16,25 +16,52 @@
 // let foundItem2 = arr.findIndex((item)=>item ===30);
 // console.log(foundItem2);
 
-let obj = {
-    firstname : "ABC",
-    lastname : "XYZ"
-}
+// let obj = {
+//     firstname : "ABC",
+//     lastname : "XYZ"
+// }
 
-let keys = Object.keys(obj);
-console.log(obj);
-console.log(keys);
+// let keys = Object.keys(obj);
+// console.log(obj);
+// console.log(keys);
 
-let values = Object.values(obj);
-console.log(values);
+// let values = Object.values(obj);
+// console.log(values);
 
-let entries = Object.entries(obj);
-console.log(entries);
+// let entries = Object.entries(obj);
+// console.log(entries);
 
-Object.freeze(obj);
+// Object.freeze(obj);
 
-obj.firstname ="Vinay";
-console.log(obj);
+// obj.firstname ="Vinay";
+// console.log(obj);
 
 // hasownProperty - true/false
 
+//Math Object
+
+// let num = Math.random();
+// console.log(num);
+
+// letFn = 10.548;
+// console.log(Math.floor());
+// console.log(Math.round());
+// console.log(Math.ceil());
+
+// console.log(Math.sqrt(17));
+// console.log(Math.pow(17));
+
+// console.log( Math.floor( ((Math.random())*10000)));
+
+// let n = Math.random();
+// console.log(Math.floor(n*10000));
+
+let otp = Math.floor(1000+ Math.random()*9000) // 1000-9999
+console.log(otp);
+
+//Destructuring
+
+let arr = [100,200,300]
+
+const[ num1 , ...newarr] = arr;
+console.log(num1,newarr);
