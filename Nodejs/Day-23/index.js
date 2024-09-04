cc73ed902639e1905eb6118db88d620187a67a61
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-04 13:36:39 +0530

    nodejs

---
+++ Nodejs/Day-23/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs"); //same as-> input fs from "fs";
+
+fs.writeFileSync("read.txt","Welcome to the first file");
+
+// let data = fs.readFileSync("read.txt");
+// console.log(data.toString());
+// console.log(data);
+console.log("After Sync Request");
+
+fs.appendFileSync("read.txt"," _Hello subahKaSitara");
+data = fs.readFileSync("read.txt");
+
+fs.renameSync("read.txt","read_write.txt");
