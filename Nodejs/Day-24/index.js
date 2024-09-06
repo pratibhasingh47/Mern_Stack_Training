81426aae9397875041aa0c309b4bcf95ca5b6c82
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-06 20:42:24 +0530

    http-request

--- Nodejs/Day-24/index.js
+++ Nodejs/Day-24/index.js
@@ -1,36 +1,90 @@
-const EventEimtter = require("events");
+// const EventEimtter = require("events");
 const http = require("http");
-const event = new EventEimtter();
+// const event = new EventEimtter();
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url == "/"){
+//         res.end("Homepage");
+//         event.emit("Home");
+//     }
+//     else if(req.url == "/about"){
+//         res.end("About ROute");
+//         event.emit("About")
+//     }
+//     else{
+//         res.end("Unknown Route");
+//         event.emit("unknownRoute")
+//     }
+// });
+
+
+
+let storedData;
 
 const server = http.createServer((req,res)=>{
-    if(req.url == "/"){
-        res.end("Homepage");
-        event.emit("Home");
+    let body = "";
+    if(req.method === "POST"){
+        req.on("data",(chunk)=>{
+            body += chunk.toString();
+        });
+        req.on("end",()=>{
+            console.log(typeof body);
+            storedData = JSON.parse(body);
+            console.log(storedData);
+            res.end(JSON.stringify(storedData));
+        })
     }
-    else if(req.url == "/about"){
-        res.end("About ROute");
-        event.emit("About")
-    }
-    else{
-        res.end("Unknown Route");
-        event.emit("unknownRoute")
-    }
-})
-
-
-server.on("request",(req,res)=>{
-    if( req.url != "/favicon.ico"){
-        console.log("Request Event",req.url);
-    }
+    // if(req.url == "/"){
+    //     res.end("Home Page");
+    //     // event.emit("homePageAccessed")
+    // }else if(req.url == "/about"){
+    //     res.end("About Route");
+    //     // event.emit("aboutPageAccessed");
+    // }else{
+    //     res.end("Unknown route");
+    //     // event.emit("unknownRouteAccessed",req.url);
+    // }
 });
 
-server.on("close",()=>{
-    console.log("Server closed");
-})
 
-setTimeout(()=>{
-    server.close();
-},5000);
+// let storedData = "";
+
+// const server = http.createServer((res, req) => {
+//     let body = "";
+//     if (req.method === "POST") {
+//         req.on("data", (chunk) => {
+//             body += chunk.toString();
+//         });
+
+//         req.on("end", () => {
+//             storedData = JSON.parse(body);
+//             console.log(storedData);
+//             res.end("Post Api called");
+//         })
+//     }
+//     if (req.url == "/") {
+//         res.end("Homepage");
+//     }
+// })
+
+
+// server.on("request",(req,res)=>{
+//     if( req.url != "/favicon.ico"){
+//         console.log("Request Event",req.url);
+//     }
+// });
+
+// server.on("close",()=>{
+//     console.log("Server closed");
+// })
+
+// setTimeout(()=>{
+//     server.close();
+// },5000);
+
+// server.on("connection", (socket) => {
+//     console.log("connection");
+// })
 
 // event.on("myName",(name,age)=>{
 //     console.log("Name is Pratibha");
@@ -64,6 +118,10 @@ setTimeout(()=>{
 //     }
 // })
 
-server.listen(300,"127.0.0.1",()=>{
-    console.log("sever is running on http://127.0.0.1:300");
+server.listen(3000, "127.0.0.1", () => {
+    console.log("sever is running on http://127.0.0.1:3000");
 })
+// new connection on every runInContext,any change in device
+
+
+
