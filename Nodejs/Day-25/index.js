905ed40b719d56a1150f96aff1433b4c5e8d5c7b
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-10 13:14:34 +0530

    express

--- Nodejs/Day-25/index.js
+++ Nodejs/Day-25/index.js
@@ -5,3 +5,31 @@ const app = express();
 app.listen(3000,()=>{
     console.log("Server is running on 3000");
 })
+
+const express = require("express");
+
+// const app = express();
+
+app.use(express.json());
+
+const users = [
+    {
+        userName : "shubham",
+        email : "shubham@gmail.com",
+        contactDetails : "1234567890"
+    }
+]
+
+app.get("/getUsers",(req,res)=>{
+    res.send({message : "User Fetched" , users : users})
+});
+
+app.post("/createUser",(req,res)=>{
+    const data = req.body;
+    users.push(data);
+    res.send({message : "User Created" , data : data});
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on 3000");
+});
