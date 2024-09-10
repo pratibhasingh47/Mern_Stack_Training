af4eaa597e3471a98625e56ffd26b3f366ac7099
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-10 20:21:53 +0530

    express.js

--- Nodejs/Day-26/index.js
+++ Nodejs/Day-26/index.js
@@ -5,12 +5,12 @@ const productRoutes = require("./router/product")
 const app = express();
 
 app.use(express.json());
-app.use("/api",userRoutes);
+app.use("/api", userRoutes);
 // app.use("/product",productRoutes);
 // app.use("/api",productRoutes);
 
 // MVC --module,view,controller
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
