bb794b0a20a368f7313eb175afde54666cf41103
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-16 09:34:04 +0530

    expressjs

--- Nodejs/Day-29/middleware/middleware.js
+++ Nodejs/Day-29/middleware/middleware.js
@@ -1,8 +1,8 @@
-const middleware = (req,res,next)=>{
+const middleware = (req, res, next) => {
     const randomNumber = Math.floor(Math.random() * 10);
-    if(randomNumber%2 !== 0){
-        return res.send({message : "Unauthorized"});
-    }else{
+    if (randomNumber % 2 !== 0) {
+        return res.send({ message: "Unauthorized" });
+    } else {
         console.log("User Authenticated");
         next();
     }
