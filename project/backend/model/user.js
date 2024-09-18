0fb456e2bd3d2fc3b2f564547d54d761e8ab1e05
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-18 21:41:22 +0530

    project

--- project/backend/model/user.js
+++ project/backend/model/user.js
@@ -2,57 +2,57 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = mongoose.Schema({
-    name : {
-        type : String,
-        required : [true , "Name is required"],
-        minlength : [ 3 , "Name should be atleast 3 char long"],
-        maxlength : [ 50 , "Name cannot exceed 50 char"],
-        validate :  {
-            validator : function(value){
-                return validator.isAlpha(value , "en-US");
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: [3, "Name should be atleast 3 char long"],
+        maxlength: [50, "Name cannot exceed 50 char"],
+        validate: {
+            validator: function (value) {
+                return validator.isAlpha(value, "en-US");
             },
-            message : "Name should be in String"
+            message: "Name should be in String"
         }
     },
-    email :{
-        type : String,
-        required : [  true , "Email is required"],
-        unique : true,
-        validate : {
-            validator : function(value){
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        validate: {
+            validator: function (value) {
                 return validator.isEmail(value);
             },
-            message : "Email should be in valid format"
+            message: "Email should be in valid format"
         }
     },
-    password :{
-        type : String,
-        required : [true , "Password is required"],
-        minlength : [ 8 , "Password should be atleast 3 char long"],
-        maxlength : [ 128 , "Password cannot exceed 128 char"],
-        validate : {
-            validator : function(value){
-                return validator.isStrongPassword(value,{
-                    minlength : 8,
-                    minLowercase : 1,
-                    minUppercase : 1,
-                    minNumbers : 1,
-                    minSymbols : 1
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password should be atleast 3 char long"],
+        maxlength: [128, "Password cannot exceed 128 char"],
+        validate: {
+            validator: function (value) {
+                return validator.isStrongPassword(value, {
+                    minlength: 8,
+                    minLowercase: 1,
+                    minUppercase: 1,
+                    minNumbers: 1,
+                    minSymbols: 1
                 })
             },
-            message : "Create a strong password"
+            message: "Create a strong password"
         }
     },
-    phoneNumber : {
-        type : String,
-        required : [true , "Enter a mobile no."],
-        validate : {
-            validator : function(value){
-                return validator.isMobilePhone(value , "en-IN")
+    phoneNumber: {
+        type: String,
+        required: [true, "Enter a mobile no."],
+        validate: {
+            validator: function (value) {
+                return validator.isMobilePhone(value, "en-IN")
             },
-            message : "Phone no. is required"
+            message: "Phone no. is required"
         }
     }
 });
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
