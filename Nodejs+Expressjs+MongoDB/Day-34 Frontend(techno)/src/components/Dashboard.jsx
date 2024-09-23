c9fbf450e4e4cda2ff4b4c5d1b9ee01b414a56eb
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-23 14:23:17 +0530

    jsx

--- Nodejs+Expressjs+MongoDB/Day-34 Frontend(techno)/src/components/Dashboard.jsx
+++ Nodejs+Expressjs+MongoDB/Day-34 Frontend(techno)/src/components/Dashboard.jsx
@@ -4,10 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment, resetCounter, updateByValue } from '../redux/slices/counterSlice';
 
 const Dashboard = () => {
-    const [users ,setUsers] = useState([]);
-    const [isAddUser , setIsAddUser] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [isAddUser, setIsAddUser] = useState(false);
     const dispatch = useDispatch();
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         try {
             const response = await fetch("http://localhost:3000/api/users");
             const data = await response.json();
@@ -25,51 +25,51 @@ const Dashboard = () => {
 
     // },[]);
 
-    const { count } = useSelector((state)=>state.counter);
+    const { count } = useSelector((state) => state.counter);
 
-    const handleIncCount = ()=>{
+    const handleIncCount = () => {
         dispatch(increment());
     }
-    const handleDecCount = ()=>{
+    const handleDecCount = () => {
         dispatch(decrement());
     }
-    const handleResetCount = ()=>{
+    const handleResetCount = () => {
         dispatch(resetCounter());
     }
-    const handleUpdateCount = ()=>{
+    const handleUpdateCount = () => {
         dispatch(updateByValue(100));
     }
 
 
-  return (
-    <div>
-        <div className='flex justify-between items-center m-5'>
+    return (
+        <div>
+            <div className='flex justify-between items-center m-5'>
 
-            <div>
-                <h1 className='text-2xl font-medium text-gray-100'>
-                    {isAddUser ?  "Add User" : "Users"}
-                </h1>
+                <div>
+                    <h1 className='text-2xl font-medium text-gray-100'>
+                        {isAddUser ? "Add User" : "Users"}
+                    </h1>
+                </div>
+                <div className='text-2xl font-medium text-white'>
+                    Counter : {count}
+                </div>
+                <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleIncCount}>Inc Count</button>
+                <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleDecCount}>Dec Count</button>
+                <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleResetCount}>Reset Count</button>
+                <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleUpdateCount}>Update Count</button>
+                <div>
+                    {
+                        isAddUser ? <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={() => {
+                            setIsAddUser(false);
+                        }}>X</button> :
+                            <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={() => {
+                                setIsAddUser(true);
+                            }}>Add User</button>
+                    }
+                </div>
             </div>
-            <div className='text-2xl font-medium text-white'>
-                Counter : {count}
-            </div>
-            <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleIncCount}>Inc Count</button>
-            <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleDecCount}>Dec Count</button>
-            <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleResetCount}>Reset Count</button>
-            <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={handleUpdateCount}>Update Count</button>
-            <div>
-                {
-                    isAddUser ? <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={()=>{
-                        setIsAddUser(false);
-                    }}>X</button> : 
-                    <button className='p-2 text-gray-100 border border-gray-100 hover:bg-gray-100 hover:text-gray-800 font-medium transition ease-in-out ' onClick={()=>{
-                        setIsAddUser(true);
-                    }}>Add User</button>
-                }
-            </div>
-        </div>
-        {/* <div className='w-[96%] mx-auto h-[69vh] bg-slate-300 rounded overflow-auto'>
-           {
+            {/* <div className='w-[96%] mx-auto h-[69vh] bg-slate-300 rounded overflow-auto'>
+            {
             isAddUser ? <UserForm setIsAddUser={setIsAddUser} fetchData={fetchData} /> : 
             (
                 <table className='text-gray-800 mx-auto w-full border-separate border-spacing-y-3 border-separate-x-2'>
@@ -111,8 +111,8 @@ const Dashboard = () => {
             )
            }
         </div> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Dashboard
