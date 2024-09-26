5976e43a66451d04465a21138b4c02a4b4b72d0a
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-26 14:05:03 +0530

    project_updated

--- project/frontend/src/components/Signup.jsx
+++ project/frontend/src/components/Signup.jsx
@@ -33,27 +33,36 @@ const Signup = () => {
 
     return (
         <div className='w-4/5 h-4/5 flex justify-center items-center bg-white shadow-2xl rounded'>
+            
             <div className='w-1/2'>
                 <img src="https://static.vecteezy.com/system/resources/previews/003/689/228/original/online-registration-or-sign-up-login-for-account-on-smartphone-app-user-interface-with-secure-password-mobile-application-for-ui-web-banner-access-cartoon-people-illustration-vector.jpg" alt="" className='w-full' />
             </div>
+
             <div className='w-1/2 flex flex-col gap-8'>
+
                 <h1 className='text-3xl text-blue-500 font-semibold text-center'>Welcome ! Sign-In</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
+
                     <div className='grid grid-cols-2 gap-3'>
+
                         <div>
                             <label className='text-blue-500 font-medium'>Name</label>
+                            <br />
                             <input type="text" className={`p-2 border w-[90%] my-2 rounded shadow-xl ${errors.name ? "border-red-500 outline-none" : "border-gray-500 outline-blue-500"}`} {...register("name")} />
                             {errors.name && (
                                 <p className='text-xs text-red-500'>{errors.name.message}</p>
                             )}
                         </div>
+
                         <div>
                             <label className='text-blue-500 font-medium'>Email</label>
+                            <br />
                             <input type="email" className={`p-2 border   w-[90%] my-2 rounded shadow-xl ${errors.email ? "border-red-500 outline-none" : "border-gray-500 outline-blue-500"}`} {...register("email")} />
                             {errors.email && (
                                 <p className='text-xs text-red-500'>{errors.email.message}</p>
                             )}
                         </div>
+
                         <div>
                             <label className='text-blue-500 font-medium'>Password</label>
                             <input type="password" className={`p-2 border w-[90%] my-2 rounded shadow-xl ${errors.password ? "border-red-500 outline-none" : "border-gray-500 outline-blue-500"}`} {...register("password")} />
@@ -61,6 +70,7 @@ const Signup = () => {
                                 <p className='text-xs text-red-500'>{errors.password.message}</p>
                             )}
                         </div>
+
                         <div>
                             <label className='text-blue-500 font-medium'>Phone Number</label>
                             <input type="number" className={`p-2 border w-[90%] my-2 rounded shadow-xl ${errors.phoneNumber ? "border-red-500 outline-none" : "border-gray-500 outline-blue-500"}`}   {...register("phoneNumber")} />
@@ -68,11 +78,16 @@ const Signup = () => {
                                 <p className='text-xs text-red-500'>{errors.phoneNumber.message}</p>
                             )}
                         </div>
+
                     </div>
+
                     <button className='bg-blue-500 my-5 text-white font-medium p-2 w-[95%] shadow-xl rounded active:bg-blue-600'>Sign-Up</button>
                 </form>
+
                 <button className='bg-red-500 my-5 text-white font-medium p-2 w-[90%] shadow-xl w-[95%] rounded active:bg-blue-600' onClick={handleGoogleLogin}>Log-In- With Google</button>
+
             </div>
+            
         </div>
     )
 }
