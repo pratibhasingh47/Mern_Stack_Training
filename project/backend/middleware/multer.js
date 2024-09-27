da51514235479e877363a4d77587e8a8ce4ad9b3
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-27 22:03:55 +0530

    project_updated

--- project/backend/middleware/multer.js
+++ project/backend/middleware/multer.js
@@ -1,11 +1,11 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination : (req , res , cb)=>{
+    destination : (req,file,cb)=>{
         cb(null,"./uploads");
     },
-    filename : ( req,res,cb)=>{
-        cb(null , `${Date.now()}-${file.originalname}`)
+    filename : (req,file,cb)=>{
+        cb(null,`${Date.now()}-${file.originalname}`)
     }
 })
 
--- project/backend/router/product.js
+++ project/backend/router/product.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getAllProduct, createProduct } = require("../controller/product");
-const upload = require("../middleware/multer")
+const { getAllProduct , createProduct } = require("../controller/product");
+const upload = require("../middleware/multer");
 const router = express.Router();
 
-router.get("/product" ,getAllProduct);
-router.post("/product", createProduct);
+router.get("/product",getAllProduct);
+router.post("/product",upload.single("productImage"),createProduct);
 
-module.exports = router;
+module.exports =  router;
