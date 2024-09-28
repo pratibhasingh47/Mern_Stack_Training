89025298197f5f7730e89236cf93650d9dc11a40
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-28 22:45:07 +0530

    project_updated

--- project/frontend/src/components/Datatable.jsx
+++ project/frontend/src/components/Datatable.jsx
@@ -4,38 +4,33 @@ import Paper from '@mui/material/Paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProduct } from '../redux/slices/productSlice';
 
+
 const paginationModel = { page: 0, pageSize: 5 };
 
-export default function DataTable({ columns }) {
-	const dispatch = useDispatch();
+export default function DataTable({columns}) {
+  const dispatch = useDispatch();
 
-	const { products, isProductAdded } = useSelector((state) => state.product);
-	console.log(products);
+  const { products ,   isProductAdded } = useSelector((state)=>state.product);
+  console.log(products);
 
-	React.useEffect(() => {
-		dispatch(getAllProduct());
-	}, []);
+  React.useEffect(()=>{
+    dispatch(getAllProduct());
+  },[]);
 
-	React.useEffect(() => {
-		if (isProductAdded) {
-			dispatch(getAllProduct());
-		}
-	}, [isProductAdded])
-
-
-	return (
-		<Paper sx={{ height: 400, width: '100%' }}>
-			<DataGrid
-				rows={products}
-				columns={columns}
-				initialState={{ pagination: { paginationModel } }}
-				pageSizeOptions={[5, 10]}
-				checkboxSelection
-				sx={{ border: 0 }}
-				// getRowId={(row) => row._id}
-			/>
-		</Paper>
-	);
-
+  React.useEffect(()=>{
+    if(isProductAdded){
+      dispatch(getAllProduct());
+    }
+  },[isProductAdded])
+  return (
+    <Paper sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={products}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  );
 }
-
