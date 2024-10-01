ce58932c5bcd234f171b4201486e5393b5feb250
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-01 19:50:29 +0530

    typescript

---
+++ TypeScript/index.js
@@ -0,0 +1,15 @@
+var greeting = "Hello, World";
+console.log(greeting);
+var age = 20;
+var isActive = true;
+var a = null;
+var b = undefined;
+var arr = [];
+var persom;
+person = ["Prats", 47];
+var x = "abc";
+var someValue = "this is a string";
+var add = function (x, y) {
+    return x + y;
+};
+console.log(add(14, 57));
---
+++ TypeScript/index.ts
@@ -0,0 +1,22 @@
+let greeting: string = "Hello, World";
+console.log(greeting);
+
+let age:number = 20;
+let isActive:boolean  = true;
+let a:null = null;
+let b:undefined = undefined;
+
+let arr:string[] = []
+
+let persom : [string , number];
+person = ["Prats" ,47 ];
+
+let x :string | number = "abc";
+
+let someValue:any = "this is a string";
+
+const add= (x:number,y:number): number=>{
+    return x+y;
+}
+
+console.log(add(14,57));
