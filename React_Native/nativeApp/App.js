ffdaa1fda13cc9adc4a7ad1bfa64de819290ff56
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-05 15:20:50 +0530

    react_native

--- React_Native/nativeApp/App.js
+++ React_Native/nativeApp/App.js
@@ -1,145 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ImageBackground, Modal, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Vector  from "./assets/splash.png"
+import { useState } from 'react';
 
 export default function App() {
+
+  const [open , setOpen] = useState(false);
+
+
   return (
-    <View style={{backgroundColor : "black" , flex : 1 , justifyContent : "center" , alignItems : "center" , padding : 60}}>
-      {/* <ImageBackground source={Vector} >
-      <Text style={{fontSize : 30 , color : "white"}}>Hello Techno</Text>
-      <Image source={Vector} style={{width : 200 , height : 200}} />
-      <Image source={{uri : "https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?cs=srgb&dl=clouds-conifer-daylight-371589.jpg&fm=jpg"}} style={{width : 200 , height : 200}} />
-      
 
-      </ImageBackground> */}
 
-      <View>
-        <ScrollView>
-        <Image source={{uri : "https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?cs=srgb&dl=clouds-conifer-daylight-371589.jpg&fm=jpg"}} style={{width : 400 , height : 400}} />
-        <Text style={{color : "white"}}>
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-        </Text>
-        <Image source={{uri : "https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?cs=srgb&dl=clouds-conifer-daylight-371589.jpg&fm=jpg"}} style={{width : 400 , height : 400}} />
-        <Text style={{color : "white"}}>
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-          hello this is techno njr
-        </Text>
-        </ScrollView>
+
+
+      <View style={{backgroundColor : "yellow", justifyContent:'center', alignItems:'center', flex:1, paddingTop:30}}>
+        <Text style={{color:"black", fontSize:25}}>Hello Prat's </Text>
+        {/* <ImageBackground source={Vector} style={{height:100, width:100}}/> */}
+        {/* <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/> */}
+        <Image source={{uri:"https://i.pinimg.com/originals/65/25/ea/6525ea3430a2145e472ce030dd98bdcb.png"}} style={{height:100, width:100}}/>
+        <Button title='Click ME' onPress={()=>{
+          console.log("Button Clicked");
+        }}/>
+        <Pressable style={{backgroundColor:'black' , padding : 10}} onPress={()=>{console.log("Pressable component")}} >
+          <Text style={{color:'white'}} >Pressable Component</Text>
+        </Pressable>
+
+        <Button title="Open Model" onPress={()=>{
+          setOpen(true);
+        }}/>
+        <Modal visible={open} animationType='slide' >
+          <Button  title='Close Modal' onPress={()=>{
+          setOpen(false);
+        }} />
+        </Modal>
+
       </View>
-    </View> 
+
+
+
   );
 }
 
